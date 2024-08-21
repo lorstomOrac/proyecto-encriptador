da154b0d2df984fb2340eda84da9e241128b6f6c
lorstomOrac <akilmor125@gmail.com>
2024-08-20 21:48:23 -0500

    actualizacion

--- proyecto.js
+++ proyecto.js
@@ -1,18 +1,93 @@
-/*
-function encriptar (){
-    var texto = document.querySelector("#input-texto").value;
-    var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    document.querySelector(".text-input-salida").value = textoCifrado;
-    document.querySelector("#input-texto").value;
-    }
-var boton1 = document.querySelector("#btn-encriptar"); boton1.onclick = encriptar;
+const textInput = document.querySelector('#frase');
+const textInputEncriptada = document.querySelector('#frase-encriptada');
 
-function desencriptar (){ var texto = document.querySelector("#input-texto").value; 
-var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
-document.querySelector(".text-input-salida").value = textoCifrado; 
-document.querySelector("#input-texto").value;
+const sugerencia = document.querySelector('.form__sugerencia');
+const desencriptarMsg = document.querySelector('.desencriptar__mensaje');
+const desencriptarResultado = document.querySelector('.desencriptar__resultado');
 
+const btnEncriptar = document.querySelector('#btn-encriptar');
+const btnDesencriptar = document.querySelector('#btn-desencriptar');
+const btnCopy = document.querySelector('#btn-copiar');
+
+const regexInput = /^[a-z0-9 ]*$/;
+
+let mensaje = true;
+let resultado = false;
+
+// Evalúa la frase usando una expresión regular
+function encriptarFrase() {
+  if (!regexInput.test(textInput.value) || textInput.value.length === 0) {
+    sugerencia.style.display = "flex";
+
+    mensaje = true;
+    mostrarMensaje()
+    resultado = false;
+    mostrarResultado();
+  } else {
+    sugerencia.style.display = "none";
+
+    let fraseEncriptada = textInput.value.
+                                          replaceAll("e", "enter").
+                                          replaceAll("i", "imes").
+                                          replaceAll("a", "ai").
+                                          replaceAll("o", "ober").
+                                          replaceAll("u", "ufat");
+
+    textInputEncriptada.value = fraseEncriptada
+
+    mensaje = false;
+    mostrarMensaje()
+    resultado = true;
+    mostrarResultado();
+
+    textInput.value = '';
+  }
+}
+btnEncriptar.addEventListener('click', encriptarFrase);
+
+// Función que copia la frase encriptada en el textarea para desencriptar
+function copy() {
+  textInputEncriptada.select();
+  document.execCommand("copy");
+  textInputEncriptada.setAttribute('disabled', true);
+  textInputEncriptada.value = '';
+  alert("El texto ha sido copiado")
+}
+btnCopy.addEventListener('click', copy);
+
+// Función inversa, para desencriptar
+function desencriptarFrase() {
+  let fraseDesencriptada = textInput.value.
+                                        replaceAll("ufat", "u").
+                                        replaceAll("ober", "o").
+                                        replaceAll("ai", "a").
+                                        replaceAll("imes", "i").
+                                        replaceAll("enter", "e");
+  textInputEncriptada.value = fraseDesencriptada;
+
+  mensaje = false;
+  mostrarMensaje()
+  resultado = true;
+  mostrarResultado();
+
+  textInput.value = '';
+}
+btnDesencriptar.addEventListener('click', desencriptarFrase);
+
+// Función que muestra el mensaje de error si no se ingresa un texto
+function mostrarMensaje() {
+  if(mensaje) {
+    desencriptarMsg.style.display = "flex";
+  } else {
+    desencriptarMsg.style.display = "none";
+  }
 }
 
-var boton2 = document.querySelector("#btn-desencriptar"); boton2.onclick = desencriptar;
-*/
+// Función que muestra el resultado
+function mostrarResultado() {
+  if(resultado) {
+    desencriptarResultado.style.display = "flex";
+  } else {
+    desencriptarResultado.style.display = "none";
+  }
+}

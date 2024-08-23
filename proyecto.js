9ce8ef9f2a471806f0073e25ff5f0069de8bea8a
lorstomOrac <akilmor125@gmail.com>
2024-08-22 22:09:10 -0500

    sin funcionar

--- proyecto.js
+++ proyecto.js
@@ -1,13 +1,13 @@
-const ingresaTexto = document.querySelector('#texto__ingresado');
-const encriptaTexto = document.querySelector('#area_desencriptada');
+const ingresaTexto = document.querySelector('.ingresar__texto');
+const encriptaTexto = document.querySelector('.lugar_de_la_frase');
 
 const mensajirijillo= document.querySelector('.mensaje');
 const mensajeDesencriptado = document.querySelector('.mensaje_desencripta');
 const resultadoDesencriptado = document.querySelector('.desencripta_frase');
 
-const botonEncripta = document.querySelector('#encriptara');
-const botonDesencripta = document.querySelector('#desencriptara');
-const botonCopear = document.querySelector('#copea');
+const botonEncripta = document.querySelector('.boton__encriptar');
+const botonDesencripta = document.querySelector('boton__desencriptar');
+const botonCopear = document.querySelector('.boton__encriptar_copiar');
 
 const mezclar = /^[a-z0-9 ]*$/;
 
@@ -15,7 +15,7 @@ let mensajito = true;
 let resultadito = false;
 
 function muestraMensajito() {
-  if(mensajes) {
+  if(mensajito) {
     mensajeDesencriptado.style.display = "flex";
   } else {
     mensajeDesencriptado.style.display = "none";
@@ -24,7 +24,7 @@ function muestraMensajito() {
 
 
 function muestraResultadito() {
-  if(resultados) {
+  if(resultadito) {
     resultadoDesencriptado.style.display = "flex";
   } else {
     resultadoDesencriptado.style.display = "none";
@@ -37,23 +37,26 @@ function encriptar() {
 
     mensajito = true;
     muestraMensajito()
-    resultado = false;
+    resultadito = false;
     muestraResultadito();
   } else {
     mensajirijillo.style.display = "none";
 
-    let encriptaFrase1 = textInput.value.
-                                          replaceAll("e", "enter").
-                                          replaceAll("i", "imes").
-                                          replaceAll("a", "ai").
-                                          replaceAll("o", "ober").
-                                          replaceAll("u", "ufat");
+    let encriptaFrase1 = textInput.value.replaceAll("e", "enter");                                      
+    let encriptaFrase2 = textInput.value.replaceAll("i", "imes");
+    let encriptaFrase3 = textInput.value.replaceAll("a", "ai");
+    let encriptaFrase4 = textInput.value.replaceAll("o", "ober");
+    let encriptaFrase5 = textInput.value.replaceAll("u", "ufat");
 
     encriptaTexto.value = encriptaFrase1
+    encriptaTexto.value = encriptaFrase2
+    encriptaTexto.value = encriptaFrase3
+    encriptaTexto.value = encriptaFrase4
+    encriptaTexto.value = encriptaFrase5
 
-    mensajes = false;
+    mensajito = false;
     muestraMensajito()
-    resultados = true;
+    resultadito = true;
     muestraResultadito();
 
     ingresaTexto.value = '';
@@ -62,17 +65,20 @@ function encriptar() {
 botonEncripta.addEventListener('click', encriptar);
 
 function desencriptar() {
-  let desencriptaFrase1 = ingresaTexto.value.
-                                        replaceAll("ufat", "u").
-                                        replaceAll("ober", "o").
-                                        replaceAll("ai", "a").
-                                        replaceAll("imes", "i").
-                                        replaceAll("enter", "e");
+  let desencriptaFrase1 = ingresaTexto.value.replaceAll("ufat", "u");                                       
+  let desencriptaFrase2 = ingresaTexto.value.replaceAll("ober", "o");
+  let desencriptaFrase3 = ingresaTexto.value.replaceAll("ai", "a");
+  let desencriptaFrase4 = ingresaTexto.value.replaceAll("imes", "i");
+  let desencriptaFrase5 = ingresaTexto.value.replaceAll("enter", "e");
   encriptaTexto.value = desencriptaFrase1;
-
-  mensajes = false;
+  encriptaTexto.value = desencriptaFrase2;
+  encriptaTexto.value = desencriptaFrase3;
+  encriptaTexto.value = desencriptaFrase4;
+  encriptaTexto.value = desencriptaFrase5;
+ 
+  mensajito = false;
   muestraMensajito();
-  resultados = true;
+  resultadito = true;
   muestraResultadito();
 
   ingresaTexto.value = '';

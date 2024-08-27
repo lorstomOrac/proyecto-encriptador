76a1187458578f8d77e4308c2fa8b1e35f123d2e
lorstomOrac <akilmor125@gmail.com>
2024-08-26 20:45:07 -0500

    esta si fue

--- proyecto.js
+++ proyecto.js
@@ -6,7 +6,7 @@ const mensajeDesencriptado = document.querySelector('.mensaje_desencripta');
 const resultadoDesencriptado = document.querySelector('.desencripta_frase');
 
 const botonEncripta = document.querySelector('.boton__encriptar');
-const botonDesencripta = document.querySelector('boton__desencriptar');
+const botonDesencripta = document.querySelector('.boton__desencriptar');
 const botonCopear = document.querySelector('.boton__encriptar_copiar');
 
 const mezclar = /^[a-z0-9 ]*$/;
@@ -33,67 +33,56 @@ function muestraResultadito() {
 
 function encriptar() {
   if (!mezclar.test(ingresaTexto.value) || ingresaTexto.value.length === 0) {
+    document.querySelector('.mensaje_desencripta').style.display = "flex";
     mensajirijillo.style.display = "flex";
-
-    mensajito = true;
-    muestraMensajito()
-    resultadito = false;
-    muestraResultadito();
   } else {
+    document.querySelector('.mensaje_desencripta').style.display = "flex";
     mensajirijillo.style.display = "none";
+    let encriptaFrase = ingresaTexto.value
+      .replaceAll("e", "enter")
+      .replaceAll("i", "imes")
+      .replaceAll("a", "ai")
+      .replaceAll("o", "ober")
+      .replaceAll("u", "ufat");
 
-    let encriptaFrase1 = textInput.value.replaceAll("e", "enter");                                      
-    let encriptaFrase2 = textInput.value.replaceAll("i", "imes");
-    let encriptaFrase3 = textInput.value.replaceAll("a", "ai");
-    let encriptaFrase4 = textInput.value.replaceAll("o", "ober");
-    let encriptaFrase5 = textInput.value.replaceAll("u", "ufat");
-
-    encriptaTexto.value = encriptaFrase1
-    encriptaTexto.value = encriptaFrase2
-    encriptaTexto.value = encriptaFrase3
-    encriptaTexto.value = encriptaFrase4
-    encriptaTexto.value = encriptaFrase5
+    encriptaTexto.value = encriptaFrase;
 
     mensajito = false;
-    muestraMensajito()
+    muestraMensajito();
     resultadito = true;
     muestraResultadito();
 
     ingresaTexto.value = "";
   }
-  
 }
-
 botonEncripta.addEventListener("click",encriptar,true);
 
 function desencriptar() {
-  let desencriptaFrase1 = ingresaTexto.value.replaceAll("ufat", "u");                                       
-  let desencriptaFrase2 = ingresaTexto.value.replaceAll("ober", "o");
-  let desencriptaFrase3 = ingresaTexto.value.replaceAll("ai", "a");
-  let desencriptaFrase4 = ingresaTexto.value.replaceAll("imes", "i");
-  let desencriptaFrase5 = ingresaTexto.value.replaceAll("enter", "e");
-  encriptaTexto.value = desencriptaFrase1;
-  encriptaTexto.value = desencriptaFrase2;
-  encriptaTexto.value = desencriptaFrase3;
-  encriptaTexto.value = desencriptaFrase4;
-  encriptaTexto.value = desencriptaFrase5;
- 
+  let desencriptaFrase = ingresaTexto.value
+    .replaceAll("ufat", "u")
+    .replaceAll("ober", "o")
+    .replaceAll("ai", "a")
+    .replaceAll("imes", "i")
+    .replaceAll("enter", "e");
+
+  encriptaTexto.value = desencriptaFrase;
+
   mensajito = false;
   muestraMensajito();
   resultadito = true;
   muestraResultadito();
 
   ingresaTexto.value = "";
-  
 }
-focus(resultadoDesencriptado);
 botonDesencripta.addEventListener("click",desencriptar,true);
 
 function copea() {
   encriptaTexto.select();
- /* document.execCommand("copy"); */
+  document.execCommand('copy'); //copea el texto en el portapapeles
   encriptaTexto.setAttribute('disabled', true);
   encriptaTexto.value = "";
   alert("se copio el mensaje")
+
 }
 botonCopear.addEventListener("click",copea,true);
+
